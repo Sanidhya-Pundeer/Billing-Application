bf200ae0d7e3cff269b966d2b7b04f53e66bb3f4
Sanidhya-Pundeer <pundeer.sanidhya@gmail.com>
2023-10-14 22:05:38 +0530

    App Added

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.static('public'))
+
+
+
+app.listen(5000, (error) => {
+    if (error) {
+        console.log("error starting server");
+    }
+    else {
+        console.log("server started at port 5000");
+    }
+});
