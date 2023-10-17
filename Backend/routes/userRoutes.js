8c2729c26d670cb6e49997492b243a63e9a96fcc
Sanidhya-Pundeer <pundeer.sanidhya@gmail.com>
2023-10-17 23:47:07 +0530

    billModel made with user payment api

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {loginController, registerController} = require('../controller/userController'); 
+const {loginController, registerController, payController} = require('../controller/userController'); 
 
 const userRouter = express.Router();
 
 userRouter.post('/login', loginController);
 userRouter.post('/register', registerController);
-
-module.exports = userRouter;
+userRouter.put('/pay/:id', payController);
+module.exports = userRouter; 
