4dff97417bd7e12bd6212bc0a7dabf7be2f14822
Riya Singh <ria_006@yahoo.com>
2023-10-18 09:52:10 +0530

    updated

--- Backend/controller/adminController.js
+++ Backend/controller/adminController.js
@@ -1,29 +1,48 @@
+const userModel = require('../model/userModel');
 const billModel = require('../model/billModel');
-const { v4 } = require('uuid');
+const encryptPassword = require('../utils/bcrypt');
 
 const billCreation = async (req, res) => {
-    const {billTitle, userEmail, billAmount} = req.body;
-    const uuid = v4();
-    const billId = uuid.slice(0, 8);
-    const billGenDate = new Date();
-    const status = 'unpaid';
-    const billDueDate = new Date();
-    billDueDate.setDate(billGenDate.getDate() + 7);
-
+    const {billId, billTitle, userEmail, billAmount, billGenDate, billDueDate, status} = req.body;
+    console.log(req.body);
     try {
         const bill = await billModel.findOne({billId});
         if(bill) {
             return res.status(400).json({message: 'Bill already exists'});
-        } else {
+        }
+        else{
             const newBill = new billModel({billId, billTitle, userEmail, billAmount, billGenDate, billDueDate, status});
             await newBill.save();
             return res.status(200).json({message: 'Bill created successfully'});
         }
     } catch (error) {
-        console.error(error);
+        console.log(error);
         res.status(500).json({message: 'Internal error'});
     }
 };
 
-module.exports = {billCreation};
+const updateBills= async(req,res)=>{
+    const id=req.params.id
+    const {billTitle, userEmail, billAmount, billDueDate, status}=req.body
+    try {
+        const task=await billModel.findByIdAndUpdate(id,{
+            title:title,
+            desc:desc,
+            doc:date,
+            userName:username
+        }, 
+        {new:true}  //return updated task
+        )
+        if (!task) {
+            res.status(404).json({message:"Task not found"})
+        } else {
+            res.status(200).json({message:"Task updated"})
+        }
+       } catch (error) {
+        res.status(500).json({message:'Server error'})
+    }
+}
+
+
+module.exports = {billCreation, updateBills}
 
